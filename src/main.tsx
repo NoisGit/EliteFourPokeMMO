06151de7b66c3b47feaeb70edef1ca6f6340cc2f
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-19 06:16:21 -0400

    Load Spanish-only UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './team-modal.css'
+import './spanish-only.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
