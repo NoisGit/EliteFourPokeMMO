7939868f36b317a22f1eb80cdec7f3516587e21f
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-19 06:20:37 -0400

    Load desktop polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './team-modal.css'
 import './spanish-only.css'
+import './desktop-polish.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
