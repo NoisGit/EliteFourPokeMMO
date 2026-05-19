84e9b78d7fb75353ec102d17ada37d49f1f28b86
Boris <108773158+NoisGit@users.noreply.github.com>
2026-05-18 22:45:49 -0400

    Remove runtime strategy translation pipeline

--- src/utils/strategyLocalization.ts
+++ src/utils/strategyLocalization.ts
@@ -1,16 +1,4 @@
 import type { Language } from '../i18n/translations'
-import { cleanEnglishStrategyText } from './englishStrategyCleanup'
-import { cleanHybridEnglishStrategyText } from './englishHybridCleanup'
-import { cleanSpanishTokensFromEnglishStrategyText } from './englishSpanishTokenCleanup'
-import { translateFullStrategyText } from './fullStrategyTranslations'
-import { formatStrategyText, translateStrategyText } from './strategyText'
+import { formatStrategyText } from './strategyFormat'
 
-export const localizeStrategyText = (text: string, language: Language) => {
-  const translatedText = translateFullStrategyText(text, language)
-  const translatedMovesText = translateStrategyText(translatedText, language)
-  const englishCleanText = cleanEnglishStrategyText(translatedMovesText, language)
-  const hybridCleanText = cleanHybridEnglishStrategyText(englishCleanText, language)
-  const localizedText = cleanSpanishTokensFromEnglishStrategyText(hybridCleanText, language)
-
-  return formatStrategyText(localizedText)
-}
+export const localizeStrategyText = (text: string, _language: Language) => formatStrategyText(text)
